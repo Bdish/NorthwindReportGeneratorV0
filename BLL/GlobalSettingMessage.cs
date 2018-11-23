8fe6649130e933637bca030bddb2d151359a1f07
pitpiter <pitpiter@mail.ru>
2018-11-23 17:03:21 +0500

    Необходимо создать веб-приложение ASP.NET MVC на C#, которое подсоединяется к базе данных. С помощью этого приложения пользователь может построить отчёт по продажам (в Excel-файл, также не лишним будет отображение на веб-странице), и отправить его на e-mail.
    Отчёт по продажам должен строиться за определенный период (предусмотреть возможность самому задавать временной диапазон) и содержать следующую информацию:
    
    номер заказа, дата заказа, артикул товара, название товара, количество реализованных единиц товара, цена реализации за единицу продукции.
    
    Excel-файл должен содержать вычисляемый столбец (столбец с формулой), который для каждой позиции должен считать стоимость.
    
    Данные для отчёта находятся в стандартной тестовой базе данных Northwind (она присоединена в виде архивированного bak-файла, нужно ее восстановить на SQL Server).
    
    Приложение будет работать в продакшене, и также планируется его дальнейшая доработка и поддержка.

---
+++ BLL/GlobalSettingMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace BLL
+{
+    public static class GlobalSettingMessage
+    {
+        public static string Host { get; set; } = "smtp.gmail.com";
+
+        public static int Port { get; set; } = 587;
+
+        public static bool EnableSsl { get; set; } = true;
+
+        public static int Timeout { get; set; } = 10000;
+
+        public static SmtpDeliveryMethod DeliveryMethod { get; set; } = SmtpDeliveryMethod.Network;
+
+        public static bool UseDefaultCredentials { get; set; } = false;
+
+        public static string  UserName{ get; set; } = "username@gmail.com";
+
+        public static string  Password{ get; set; } = "password";    
+    }
+}
---
+++ BLL/MessageManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace BLL
+{
+    public class MessageManager
+    {
+
+      /*  public MessageManager()
+        {
+
+        }*/
+
+        public void SendMsgWithFile(string email,string subject,string textBody ,Attachment attachment)
+        {
+            SmtpClient client = new SmtpClient(GlobalSettingMessage.Host, GlobalSettingMessage.Port);
+            client.EnableSsl = GlobalSettingMessage.EnableSsl; 
+            client.Timeout = GlobalSettingMessage.Timeout;
+            client.DeliveryMethod = GlobalSettingMessage.DeliveryMethod;
+            client.UseDefaultCredentials = GlobalSettingMessage.UseDefaultCredentials;
+            client.Credentials = new System.Net.NetworkCredential(GlobalSettingMessage.UserName, GlobalSettingMessage.Password);
+            MailMessage msg = new MailMessage();
+            msg.To.Add(email);
+            msg.From = new MailAddress(GlobalSettingMessage.UserName);
+            msg.Subject = subject;
+            msg.Body = textBody;
+            msg.Attachments.Add(attachment);
+            client.Send(msg);
+        }
+    }
+}
